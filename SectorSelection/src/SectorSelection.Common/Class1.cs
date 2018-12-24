9ab226a6dc6068e11a0b4febcbef9be67ab2811d
Demirci, Baris <baris.demirci@accenture.com>
2018-12-24 19:24:31 +0200

    Projects are created

---
+++ SectorSelection/src/SectorSelection.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SectorSelection.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ SectorSelection/test/ServiceLayer/SectorSelection.Repositories.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SectorSelection.Repositories.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
