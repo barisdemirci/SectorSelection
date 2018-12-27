aa371859b48ff72981ae9b5dad890b75b9ecf202
Demirci, Baris <baris.demirci@accenture.com>
2018-12-28 00:30:33 +0200

    Unit Tests for WebApi projects

---
+++ SectorSelection/test/ServiceLayer/SectorSelection.WebApi.Tests/Controllers/SectorControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using SectorSelection.Dtos;
+using SectorSelection.Dtos.Builder;
+using SectorSelection.Services.Sector;
+using SectorSelection.WebApi.Controllers;
+using Xunit;
+
+namespace SectorSelection.WebApi.Tests.Controllers
+{
+    public class SectorControllerTests
+    {
+        private readonly SectorController sectorController;
+        private readonly ISectorService sectorService;
+
+        public SectorControllerTests()
+        {
+            sectorService = Substitute.For<ISectorService>();
+            sectorController = new SectorController(sectorService);
+        }
+
+        [Fact]
+        public void Constructor_ArgumentIsNull_ThrowsArgumentNullException()
+        {
+            // act & assert
+            Assert.Throws<ArgumentNullException>(() => new SectorController(null));
+        }
+
+        [Fact]
+        public async Task GetSectorsAsync_CallsService()
+        {
+            // act
+            await sectorController.GetSectorsAsync();
+
+            // assert
+            await sectorService.Received(1).GetSectorsAsync();
+        }
+
+        [Fact]
+        public async Task SaveSelectedSectorsAsync_CallsService()
+        {
+            // arrange
+            SaveSelectedSectorsDto dto = SaveSelectedSectorsDtoBuilder.Build();
+
+            // act
+            await sectorController.SaveSelectedSectorsAsync(dto);
+
+            // assert
+            await sectorService.Received(1).SaveSelectedSectorsAsync(dto);
+        }
+    }
+}
---
+++ SectorSelection/test/ServiceLayer/SectorSelection.WebApi.Tests/Controllers/UserSectorsControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using NSubstitute;
+using SectorSelection.Services.UserSectors;
+using SectorSelection.WebApi.Controllers;
+using System;
+using Xunit;
+
+namespace SectorSelection.WebApi.Tests.Controllers
+{
+    public class UserSectorsControllerTests
+    {
+        private readonly UserSectorsController userSectorsController;
+        private readonly IUserSectorsService userSectorsService;
+
+        public UserSectorsControllerTests()
+        {
+            userSectorsService = Substitute.For<IUserSectorsService>();
+            userSectorsController = new UserSectorsController(userSectorsService);
+        }
+
+        [Fact]
+        public void Constructor_ArgumentIsNull_ThrowsArgumentNullException()
+        {
+            // act & assert
+            Assert.Throws<ArgumentNullException>(() => new UserSectorsController(null));
+        }
+
+        [Fact]
+        public void GetUserSectorsAsync_CallsService()
+        {
+            // act
+            userSectorsService.GetUserSectors();
+
+            // assert
+            userSectorsService.Received(1).GetUserSectors();
+        }
+    }
+}
